5584a0b24fab21205265a4648cb7f5868115c702
whmnet <whmnet@yahoo.com.br>
2022-11-07 11:15:15 -0300

    criando class livros

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -7,8 +7,44 @@ public class PrimeiroPrograma {
         Gato gato = new Gato();
         System.out.println(gato);
 
+        Livros livro1 = new Livros( "o problema dos 3 corpos", 300);
+        System.out.println(livro1);
+
        /*  int a = 3;
         int b = 2;
         System.out.println("Hello world ! " + (a+b));*/
+    }
+}
+class Livros{
+    private String nome;
+    private Integer numpaginas;
+
+    public Livros(String nome, Integer numpaginas) {
+        this.nome = nome;
+        this.numpaginas = numpaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumpaginas() {
+        return numpaginas;
+    }
+
+    public void setNumpaginas(Integer numpaginas) {
+        this.numpaginas = numpaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livros{" +
+                "nome='" + nome + '\'' +
+                ", numpaginas=" + numpaginas +
+                '}';
     }
 }
