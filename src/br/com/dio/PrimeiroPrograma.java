cf875858d6edfb5ad9f537be4a4f3105b7c96209
whmnet <whmnet@yahoo.com.br>
2022-11-07 10:07:09 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+package br.com.dio;
+
+ import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+       /*  int a = 3;
+        int b = 2;
+        System.out.println("Hello world ! " + (a+b));*/
+    }
+}
